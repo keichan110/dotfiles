5797dba348973322da3d4c3c0e1227423f5e17c4
ito-ito <ittooco@gmail.com>
2026-05-17 07:40:31 +0900

    nvim: VSCode 設定に合わせた エディタオプション と 自動処理を実装

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,28 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Trim trailing whitespace on save (VSCode: files.trimTrailingWhitespace)
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = vim.api.nvim_create_augroup("trim_trailing_whitespace", { clear = true }),
+  pattern = "*",
+  callback = function()
+    if vim.bo.modifiable then
+      vim.cmd("%s/\\s\\+$//e")
+    end
+  end,
+})
+
+-- Trim final newlines on save (VSCode: files.trimFinalNewlines)
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = vim.api.nvim_create_augroup("trim_final_newlines", { clear = true }),
+  pattern = "*",
+  callback = function()
+    if vim.bo.modifiable then
+      local lastline = vim.fn.getline("$")
+      if lastline == "" then
+        vim.cmd("silent! $delete")
+      end
+    end
+  end,
+})
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,52 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- VSCode settings alignment
+local opt = vim.opt
+
+-- Indentation and tabs
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- Line wrapping
+opt.wrap = true
+opt.textwidth = 120
+opt.linebreak = true
+
+-- Display
+opt.number = true
+opt.relativenumber = true
+opt.scrolloff = 15
+opt.sidescrolloff = 5
+opt.cursorline = false -- VSCode: renderLineHighlight = "none"
+opt.smoothscroll = true
+
+-- Whitespace and special characters
+opt.listchars = {
+  tab = "→ ",
+  trail = "·",
+  nbsp = "␣",
+  extends = "»",
+  precedes = "«",
+}
+opt.list = true
+
+-- Cursor
+opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50"
+
+-- Font (for GUI neovim like Neovide, GNvim)
+opt.guifont = "PlemolJP:h14.5,HackGen:h14.5,Cica:h14.5"
+opt.linespace = 5 -- lineHeight: 1.85 to approximate linespace
+
+-- File handling
+opt.autowrite = false
+opt.autowriteall = false
+
+-- Search
+opt.ignorecase = true
+opt.smartcase = true
+opt.incsearch = true
+opt.hlsearch = true
