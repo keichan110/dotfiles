56d98ae404a1e2ab7b8fe9819758471ec571af2c
ito-ito <ittooco@gmail.com>
2026-05-27 21:42:27 +0900

    nvim: インデントハイライトの色味調整

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,6 +2,20 @@ return {
   -- Add nord theme
   {
     "shaunsingh/nord.nvim",
+    init = function()
+      -- カラースキーム適用後にインデントラインのハイライトを調整
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        callback = function()
+          vim.schedule(function()
+            local bg = vim.fn.synIDattr(vim.fn.hlID("Normal"), "bg#")
+            -- 非アクティブなインデントラインを背景色と同色にして非表示化
+            vim.api.nvim_set_hl(0, "SnacksIndent", { fg = bg })
+            -- アクティブなスコープラインのトーンを落とす
+            vim.api.nvim_set_hl(0, "SnacksIndentScope", { fg = "#4C566A" })
+          end)
+        end,
+      })
+    end,
   },
 
   -- Configure LazyVim to load nord
