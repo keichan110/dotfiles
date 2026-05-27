3d961a42134bfc5de11a5fe55c65fcfee2b99841
ito-ito <ittooco@gmail.com>
2026-05-27 22:10:37 +0900

    nvim: 検索結果を画面中央に表示

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -7,5 +7,10 @@ vim.keymap.set({ "n", "i", "v" }, "<leader>w", "<cmd>w<cr>", { desc = "Save file
 vim.keymap.set("n", "x", '"_x', { desc = "Delete char without yanking" })
 vim.keymap.set("n", "s", '"_s', { desc = "Delete char and insert without yanking" })
 
+vim.keymap.set("n", "n", "'Nn'[v:searchforward].'zz'", { expr = true, desc = "Next Search Result" })
+vim.keymap.set("n", "N", "'nN'[v:searchforward].'zz'", { expr = true, desc = "Prev Search Result" })
+vim.keymap.set("n", "*", "*zz", { desc = "Search word under cursor and center" })
+vim.keymap.set("n", "#", "#zz", { desc = "Search word under cursor backward and center" })
+
 vim.keymap.set("n", ";", ":", { desc = "Enter command mode" })
 vim.keymap.set("n", ":", ";", { desc = "Repeat f/t motion" })
